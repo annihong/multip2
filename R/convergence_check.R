1e38e33a3dd2beddf5164bdb0f944396c562e246
annihong <annihong7153@gmail.com>
2024-06-12 11:51:37 -0400

    convergence checks bug fix

--- R/convergence_check.R
+++ R/convergence_check.R
@@ -2,7 +2,7 @@
 #'
 #' This function checks the convergence of a fitted Stan model for a multiplex network using the ggmcmc package.
 #'
-#' @param multip2fit the fitted MultiP2Fit object.
+#' @param model.obj a Mp2Model object
 #' @param params A character string indicating which parameters to plot. Must be one of "fixed" (all the fixed parameters), "random"(actor random effects), or "custom" (use the custom_params argument)
 #' @param custom_params A character string indicating a custom regular expression to match parameter names to plot, as given by a character vector or a regular expression, i.e., the "family" parameter in ggmcmc::ggs.
 #' @param plot A character vector containing the names of the desired plots.m Default is c("traceplot", "running", or "geweke")
@@ -10,9 +10,9 @@
 #' @param file A character string indicating the output file name for the plots, default = "ggmcmc-output.pdf"
 #' @return NULL
 #' @export
-convergence_check <- function(multip2fit, params, custom_params="", plot=c("traceplot", "running", "geweke", "density"), plot_all = FALSE, file = "ggmcmc-output.pdf") {
-    fit <- multip2fit$stan_fit
-    par_labels <- multip2fit$par_labels
+convergence_check <- function(model.obj, params, custom_params="", plot=c("traceplot", "running", "geweke", "density"), plot_all = FALSE, file = "ggmcmc-output.pdf") {
+    fit <- model.obj$fit_res$stan_fit
+    par_labels <- model.obj$fit_res$par_labels
 
     if (params == "fixed") {
         S <- ggmcmc::ggs(fit, family = "^mu|^rho|^cross|fixed", par_labels = par_labels, keep_original_order=TRUE, sort=FALSE)
